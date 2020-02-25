f31762c9219f72735563d0351914addb303a75d2
FlowOverstack <ajdika@live.com>
2020-02-25 12:44:46 +0100

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# vue-003-vue-code-is-easy
-Created with CodeSandbox
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <ul>
+      <!-- Elements in iteration expect to have 'v-bind:key' directives. -->
+      <!-- avoid using non-primitive value as key: v-bind:key="entry" -->
+      <li v-for="entry in entries" class="list-item" v-bind:key="entry.content">{{ entry.content }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      entries: [{ content: "a" }, { content: "b" }, { content: "c" }]
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.list-item {
+  background: white;
+  color: gray;
+  padding: 20px;
+  margin: 20px;
+}
+ul {
+  background: tomato;
+  width: 400px;
+  height: 400px;
+}
+</style>
